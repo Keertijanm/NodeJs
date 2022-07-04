59dd1f6ec6d4bf925a377f4c6f3e112d65b5f921
Keertijanm <81510729+Keertijanm@users.noreply.github.com>
2022-07-04 09:03:22 +0530

    New commit

---
+++ server.js
@@ -0,0 +1,26 @@
+// console.log("this is server.js");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.listen(3000, function () {
+  console.log("listening on 3000");
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+  // do something here
+  console.log("get method call");
+   res.send("Hello World");
+});
+
+app.post("/quotes", (req, res) => {
+  console.log("Hellooooooooooooooooo!");
+   console.log(req.body);
+  res.send("Hello World");
+});
+
+const MongoClient = require("mongodb").MongoClient;
+MongoClient.connect("mongodb-connection-string", (err, client) => {
+  // ... do something here
+  console.log("this is mongodb server");
+});
